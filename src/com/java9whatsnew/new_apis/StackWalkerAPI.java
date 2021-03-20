4658ec32bada756760a8cc0a08b9dc143bbd7398
Joao Lopes <joao.lopes+joao.lopes@elo7.com>
2021-03-20 19:20:58 -0300

    new apis: StackWalker

--- src/com/java9whatsnew/new_apis/StackWalkerAPI.java
+++ src/com/java9whatsnew/new_apis/StackWalkerAPI.java
@@ -8,28 +8,9 @@ import java.util.List;
 public class StackWalkerAPI {
 
 	public static void main(String[] args) {
-		
 		method1();
-		
-		// walk the stack before java 9
-		// esse metodo de andar pela stack tem problemas de performance, tbm nao permite que voce limite a profundidade
-		// que voce quer da stack, e nao tem garantia de que voce esta vendo TODOS elementos da stack pq a JVM pode omitir alguns
-		StackTraceElement[] stackTraceElements = 
-			new Throwable().getStackTrace();
-		
-		// metodo java 9: melhor performance, garante que voce vera toda stack
-		StackWalker stackWalker = StackWalker.getInstance();
-		
-		stackWalker.forEach(stackElement -> System.out.println(stackElement.getMethodName()));
-		
-		List<Integer> lines = stackWalker.walk(stackStream -> 
-			stackStream.filter(f -> f.getMethodName().startsWith("m"))
-				.map(StackFrame::getLineNumber)
-				.collect(toList())
-			);
-		
 	}
-	
+
 	public static void method1() {
 		method2();
 	}
@@ -39,7 +20,30 @@ public class StackWalkerAPI {
 	}
 
 	private static void method3() {
+		method4();
+	}
+
+	private static void method4() {
+
+		// walk the stack before java 9
+		// esse metodo de andar pela stack tem problemas de performance, tbm nao permite
+		// que voce limite a profundidade
+		// que voce quer da stack, e nao tem garantia de que voce esta vendo TODOS
+		// elementos da stack pq a JVM pode omitir alguns
+		@SuppressWarnings("unused")
+		StackTraceElement[] stackTraceElements = new Throwable().getStackTrace();
+
+		// metodo java 9: melhor performance, garante que voce vera toda stack
+		StackWalker stackWalker = StackWalker.getInstance();
+
+		stackWalker.forEach(System.out::println);
+
+		List<Integer> lines = stackWalker.walk(stackStream -> stackStream.filter(f -> f.getMethodName().startsWith("m"))
+				.map(StackFrame::getLineNumber)
+				.collect(toList()));
+
+		lines.forEach(System.out::println);
 		
 	}
-	
+
 }
